4338e372d81744abf780f24b4703f9088cb616e0
Saranya <jsaran.bj@gmail.com>
2024-03-19 15:07:43 +0100

    Swap

---
+++ swap.py
@@ -0,0 +1,12 @@
+a=int(input("Enter value of a "))
+b=int(input("Enter value of b "))
+print("before swapping:")
+print("a:",a)
+print("b:",b)
+c=a
+a=b
+b=c
+
+print("After Swapping:")
+print("a:",a)
+print("b:",b)
